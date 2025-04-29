05fcf2a3207d83c937ed0fb722f887ef1c48d8d9
Lutho-Mbooi <134628021+Lutho-Mbooi@users.noreply.github.com>
2025-04-29 02:24:18 +0200

    Create datatypes_operators.js

---
+++ datatypes_operators.js
@@ -0,0 +1,34 @@
+//  Numbers
+let integerNum = 7;
+let floatNum = 3.5;
+console.log(integerNum + 93);
+console.log(integerNum - 6);
+console.log(integerNum * 10);
+console.log(integerNum / 2);
+console.log(integerNum % 2);
+console.log(integerNum ** 2);
+
+// Boolean and Operators
+let isGreater = 8 > 12;
+let isLessOrEqual = 8 <= 12;
+let isEqual = 8 === 12;
+let isNotEqual = 8 !== 12;
+console.log(isGreater, isLessOrEqual, isEqual, isNotEqual);
+
+let a = true;
+let b = false;
+console.log(a && b);
+console.log(a || b);
+console.log(!a);
+
+let p = 10;
+p += 5;
+console.log(p);
+p -= 3;
+console.log(p);
+p *= 2;
+console.log(p);
+p /= 4;
+console.log(p);
+p %= 3;
+console.log(p);
