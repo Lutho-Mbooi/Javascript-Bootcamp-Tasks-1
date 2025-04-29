6caf1abeaf1a8c8bb80de01139de35d199ee89a0
Lutho-Mbooi <134628021+Lutho-Mbooi@users.noreply.github.com>
2025-04-29 02:30:26 +0200

    Update variables.js

--- variables.js
+++ variables.js
@@ -2,3 +2,18 @@ let name = "Mbooi Lutholwethu";
 let age = 23;
 let isStudent = false;
 console.log(name, age, isStudent);
+
+//  Variable Reassignment
+let favoriteColour = "Blue";
+console.log(favoriteColour);
+favoriteColour = "Green";
+console.log(favoriteColour);
+
+//  Arithmetic Operations
+let num1 = 10;
+let num2 = 5;
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+console.log(sum, difference, product, quotient);
